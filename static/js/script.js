c8e82e8d4e410231d3e634810941c8528210678a
abasha2002 <abashishvili2002@gmail.com>
2023-09-10 23:26:41 +0400

    update

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
     const productList = document.querySelector(".product-list");
+    const productCount = document.getElementById("product-count");
+    const cartIcon = document.getElementById('cart-icon');
+    const cartPanel = document.createElement('div');
+    cartPanel.classList.add('cart-panel');
+
+    // Create a close button (X icon)
+    const closeButton = document.createElement('div');
+    closeButton.classList.add('close-button');
+    closeButton.innerHTML = 'X';
+
+    // Append the close button to the cart panel
+    cartPanel.appendChild(closeButton);
+
+    const OffCanvasDiv = document.createElement("div");
+    OffCanvasDiv.classList.add("OffCanvas-body");
+
+
+    // Create the TitleOffcanvas
+    const TitleOffcanvas = document.createElement("h3");
+    TitleOffcanvas.textContent = `Products:`;
+    TitleOffcanvas.style.textAlign = "center";
+    TitleOffcanvas.style.marginBottom = "0";
+
+    // Create a container for the final price
+    const finalPriceContainer = document.createElement("div");
+    finalPriceContainer.classList.add("final-price");
+    finalPriceContainer.innerHTML = `
+        <div class="d-flex position-absolute justify-content-center align-items-center" style="bottom: 10px;">
+            <h2 class="final-price-text">Final Price: $0</h2>
+        </div>
+    `;
+    
+    cartPanel.appendChild(TitleOffcanvas);
+    cartPanel.appendChild(OffCanvasDiv);
+    cartPanel.appendChild(finalPriceContainer);
+
+    // Initialize the total price variable
+    let totalPrice = 0;
+
+    // Append the cart panel to the body
+    document.body.appendChild(cartPanel);
+
     fetch("https://fakestoreapi.com/products")
         .then((response) => response.json())
         .then((data) => {
@@ -22,6 +64,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 // Create title of the item
                 const title = document.createElement("h2");
+                title.classList.add("product-title");
                 title.textContent = product.title;
 
                 productItem.appendChild(title);
@@ -41,8 +84,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 // calculate new price
                 const newPrice = document.createElement("strong");
-                newPrice.textContent = `$${product.price - (product.price * 20 / 100)}`;
+                newPrice.classList.add("product-price");
+                newPrice.textContent = `$${(product.price - (product.price * 20 / 100)).toFixed(2)}`;
 
+                // Add a click event listener to the product item
+                productItem.addEventListener("click", (event) => {
+                    const target = event.target;
+
+                    if (target.classList.contains("first-icon")) {
+                        // This is the first icon, you can handle it separately
+                        // Get the current count
+                        let count = parseInt(productCount.textContent);
+                        // Increment the count by one
+                        count += 1;
+                        // Update the count in the element
+                        productCount.textContent = count;
+
+                        // Add the product information to the cart
+                        const cartItem = document.createElement("div");
+                        cartItem.classList.add("cart-item");
+                        cartItem.innerHTML = `
+                            <div class="mt-4 d-flex align-items-center justify-content-between">
+                                <h3 class="cart-product-title">${product.title}</h3>
+                                <strong class="cart-product-price">$${(product.price - (product.price * 20 / 100)).toFixed(2)}</strong>
+                            </div>
+                        `;
+
+                        OffCanvasDiv.appendChild(cartItem);
+                        
+                        totalPrice += (product.price - (product.price * 20 / 100));
+                        finalPriceContainer.querySelector(".final-price-text").textContent = `Final Price: $${totalPrice.toFixed(2)}`;
+                    }
+                });
 
                 //add old price and new price into spanDiv
                 spanDiv.appendChild(price);
@@ -58,68 +131,56 @@ document.addEventListener("DOMContentLoaded", () => {
         .catch((error) => {
             console.error("Error fetching data: ", error);
         });
-});
 
-function addImagesToProductImages() {
-    const productImageDivs = document.querySelectorAll(".product-image");
+    // Function to add images to .product-image divs
+    function addImagesToProductImages() {
+        const productImageDivs = document.querySelectorAll(".product-image");
 
-    // Define the URLs for your icons
-    const iconUrls = [
-        "static/images/Add-To-Cart.svg",
-        "static/images/Wish-List.svg",
-    ];
+        // Define the URLs for your icons
+        const iconUrls = [
+            "static/images/Add-To-Cart.svg",
+            "static/images/Wish-List.svg",
+        ];
 
-    productImageDivs.forEach((imageDiv) => {
-        iconUrls.forEach((iconUrl, index) => {
-            // Create an image element for each icon
-            const iconImg = document.createElement("img");
-            iconImg.src = iconUrl;
-            iconImg.alt = "icon to add item in your cart"; // Set an appropriate alt text
+        productImageDivs.forEach((imageDiv) => {
+            iconUrls.forEach((iconUrl, index) => {
+                // Create an image element for each icon
+                const iconImg = document.createElement("img");
+                iconImg.src = iconUrl;
+                iconImg.alt = "icon to add item in your cart"; // Set an appropriate alt text
 
-            // Apply styles to the icons
-            iconImg.style.position = "absolute";
-            iconImg.style.top = "10px";
-            iconImg.style.right = "20px";
-            iconImg.style.zIndex = "2";
-            iconImg.style.width = "38px";
-            iconImg.style.height = "38px";
-            iconImg.style.cursor = "pointer";
+                // Apply styles to the icons
+                iconImg.style.position = "absolute";
+                iconImg.style.top = "10px";
+                iconImg.style.right = "20px";
+                iconImg.style.width = "38px";
+                iconImg.style.height = "38px";
+                iconImg.style.cursor = "pointer";
 
-            if(index == 0){
-                iconImg.classList.add("first-icon");
-            }
+                if (index == 0) {
+                    iconImg.classList.add("first-icon");
+                }
 
-            if (index === 1) {
-                iconImg.style.top = "56px";
-            }
+                if (index === 1) {
+                    iconImg.style.top = "56px";
+                }
 
-            // Append the icon image to the .product-image div
-            imageDiv.appendChild(iconImg);
+                // Append the icon image to the .product-image div
+                imageDiv.appendChild(iconImg);
+            });
         });
-    });
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    function handleIconClick(event) {
-        const target = event.target;
-        const productCount = document.getElementById("product-count");
-
-        // Check if the clicked element is an icon within a product-image div
-        if (target.tagName === "IMG" && target.parentNode.classList.contains("product-image")) {
-            if (target.classList.contains("first-icon")) {
-                // This is the first icon, you can handle it separately
-                // Get the current count
-                let count = parseInt(productCount.textContent);
-                // Increment the count by one
-                count += 1;
-                // Update the count in the element
-                productCount.textContent = count;
-            } else {
-                // This is the second icon, handle it differently if needed
-            }
-        }
     }
 
-    // Add a click event listener to the document and delegate the handling
-    document.addEventListener("click", handleIconClick);
+    // Function to create and open the cart panel
+    function createAndOpenCartPanel() {
+        cartPanel.classList.add('open');
+    }
+
+    // Add event listener to close the cart panel when the X button is clicked
+    closeButton.addEventListener('click', () => {
+        cartPanel.classList.remove('open');
+    });
+
+    // Add event listener to open the cart panel when the cart icon is clicked
+    cartIcon.addEventListener('click', createAndOpenCartPanel);
 });
